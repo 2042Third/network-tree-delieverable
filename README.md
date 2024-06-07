7bb665447a8ace5ec6c3304e4e38e6ad40483409
mikeyang <18604713262@163.com>
2024-06-07 18:00:31 -0400

    Server interactions.

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 1. Clone the repository
 2. Run `npm install`
 3. Run `docker build -t <image-name> .`
-4. Run `docker run -p 3000:3000 <image-name>`, which starts the docker.
+4. Run `docker run -p 3000:3000 <image-name>`, which starts the docker and continuously monitor the stdout. Or run `docker run -d -p 3000:3000 <image-name>` to run the docker in the background.
 5. Run `docker logs -f <container-id>` to get the stdout of the docker.
--- app.js
+++ app.js
@@ -3,15 +3,18 @@
  * Used for Network Tree SDE internship application.
  * */
 
+const { states_db } = require('./db.js');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
+let states_set = new Set();
+
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
-
 
 
 // Handle POST request
@@ -24,12 +27,31 @@ app.post('/data', async (req, res) => {
       JSON.stringify({
           state: data.state,
           status: "success",
-          message: "Data received",
+          message: states_set.has(data.state)?"Input state exists in the database.": "Input state doesn't exist in the database",
       })
     );
 });
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+// Handle POST request to 'states' endpoint
+app.post('/states', async (req, res) => {
+    let data = await req.body;
+    console.log("Sending all states.");
+    res.status(200).send(
+      JSON.stringify({
+          states : states_db,
+      })
+    );
 });
+
+const init = async () => {
+  states_set = new Set(states_db);
+};
+
+// Start the server
+init()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on http://localhost:${port}`);
+    });
+  })
+  .catch(console.error);
